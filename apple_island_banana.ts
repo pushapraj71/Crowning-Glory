309c9cee8cc8f035aad31942fd5d125661e0f725
pushapraj71 <136750844+pushapraj71@users.noreply.github.com>
2023-07-17 19:12:42 +0700

    Refactor business logic

---
+++ apple_island_banana.ts
@@ -0,0 +1,42 @@
+//Initializing Variables
+let head: string = "";
+let body: string = "";
+let legs: string = "";
+let feet: string = "";
+let hands: string = "";
+
+//Building the Crown
+head += "  *  "; 
+head += "\n *_* "; 
+head += "\n|* *|"; 
+head += "\n|*_*|"; 
+
+//Building the Body
+body += "\n|___|"; 
+body += "\n|___|"; 
+body += "\n|___|"; 
+
+//Building the Legs
+legs += "\n|_*_|"; 
+legs += "\n|___|"; 
+legs += "\n|___|"; 
+
+//Building the Feet
+feet += "\n|___|"; 
+feet += "\n|___|"; 
+feet += "\n|*_*|"; 
+
+//Building the Hands
+hands += "\n|___|"; 
+hands += "\n|_*_|"; 
+hands += "\n|___|"; 
+
+//Printing the Crown
+console.log(head);
+console.log(body);
+console.log(legs);
+console.log(feet);
+console.log(hands);
+
+//Crowning Glory
+console.log("\nCrowning Glory");
